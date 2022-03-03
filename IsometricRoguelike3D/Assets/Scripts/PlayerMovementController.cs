ccf83fd38f900aec8bd6fffa2bc13ed790985e50
Egemen Turoglu <62505792+EgmTrg@users.noreply.github.com>
2022-03-03 20:41:59 +0300

    +added: Camera following and rotation control

--- IsometricRoguelike3D/Assets/Scripts/PlayerMovementController.cs
+++ IsometricRoguelike3D/Assets/Scripts/PlayerMovementController.cs
@@ -6,20 +6,36 @@ namespace IsometricRoguelike.Player.Movement
 
     public class PlayerMovementController : MonoBehaviour
     {
-        [SerializeField] private Rigidbody _rigidbody;
+        [SerializeField] private CharacterController _characterController;
         [SerializeField] private PlayerMovementSettings _settings;
+        [SerializeField] private Transform _cameraTransform;
 
-        // Start is called before the first frame update
+        private float smoothVelocity;
+        private float smoothTime = 0.1f;
 
-        void Start()
-        {
-            _rigidbody = GetComponent<Rigidbody>();
-        }
-
-        // Update is called once per frame
         void FixedUpdate()
         {
-            _rigidbody.velocity = new Vector3(InputData.Movement.Horizontal, 0, InputData.Movement.Vertical) * _settings.PlayerSpeed * 100 * Time.deltaTime;
+            Movement();
         }
+
+        private void Movement()
+        {
+            Vector3 direction = new Vector3(InputData.Movement.Horizontal, 0, InputData.Movement.Vertical).normalized;
+            if (direction.magnitude >= 0.1f)
+            {
+                float targetAngle = RotationToDirection(direction);
+                Vector3 moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
+                _characterController.Move(moveDir.normalized * _settings.PlayerSpeed * Time.deltaTime);
+            }
+        }
+
+        private float RotationToDirection(Vector3 direction)
+        {
+            float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + _cameraTransform.eulerAngles.y;
+            float angle = Mathf.SmoothDampAngle(transform.rotation.eulerAngles.y, targetAngle, ref smoothVelocity, smoothTime);
+            transform.rotation = Quaternion.Euler(0f, angle, 0f);
+            return targetAngle;
+        }
+
     }
 }
