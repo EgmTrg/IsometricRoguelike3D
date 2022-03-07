0fdaf5fcca02c1a41614ecac8c252aead0088eca
Egemen Turoglu <62505792+EgmTrg@users.noreply.github.com>
2022-03-07 17:55:35 +0300

    +added: If hits player to chest, its changing color for now.

---
+++ IsometricRoguelike3D/Assets/Scripts/Interactable/Collectable/Chest.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+namespace IsometricRoguelike.Interactable.Collectable
+{
+    public class Chest : CollectableObjects
+    {
+        protected override void Awake()
+        {
+            base.Awake();
+        }
+
+        protected override void OnCollect()
+        {
+            base.OnCollect();
+            // animation plays.
+        }
+
+        protected override void AfterCollect()
+        {
+            base.AfterCollect();
+            // Adds or spawn collected item then destroy it.
+            //Destroy(gameObject);
+        }
+    }
+}
