12ab5497ceb6ccc057f6802c97f93dad0802aaeb
Egemen Turoglu <62505792+EgmTrg@users.noreply.github.com>
2022-03-08 18:46:28 +0300

    +created: Combat temporary files.

---
+++ IsometricRoguelike3D/Assets/Scripts/CombatManager.cs
@@ -0,0 +1,13 @@
+﻿using IsometricRoguelike.Entities;
+using UnityEngine;
+
+namespace IsometricRoguelike.Combat
+{
+    public static class CombatManager
+    {
+        public static void TakeDamage(this Entity entity,int damage)
+        {
+            Debug.LogWarning("TakeDamage metot bos!");
+        }
+    }
+}
