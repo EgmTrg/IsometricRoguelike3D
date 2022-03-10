41189ecd338d77b1afb15b0972c2885988e91552
Egemen Turoglu <62505792+EgmTrg@users.noreply.github.com>
2022-03-10 11:47:52 +0300

    +added: HealthManager and Settings created. +created: ChestHealth and PlayerHealth Settings.

---
+++ IsometricRoguelike3D/Assets/Scripts/Combat/HealthManager.cs
@@ -0,0 +1,30 @@
+﻿using IsometricRoguelike.Singletons;
+using UnityEngine;
+
+namespace IsometricRoguelike.Health
+{
+    public class HealthManager : MonoSingleton<HealthManager>
+    {
+        private HealthSettings entityHealthSettings = null;
+
+        public HealthManager(HealthSettings settings)
+        {
+            if (entityHealthSettings != settings || entityHealthSettings == null)
+                entityHealthSettings = settings;
+        }
+
+        public void TakeDamage(int damage)
+        {
+            var health = Mathf.Clamp(entityHealthSettings.Health_Current - damage, entityHealthSettings.MinimumHealth, entityHealthSettings.Health_Max);
+            CheckAlive(health);
+        }
+
+        private void CheckAlive(float health)
+        {
+            if (health == entityHealthSettings.MinimumHealth)
+            {
+                entityHealthSettings.IsAlive = false;
+            }
+        }
+    }
+}
---
+++ IsometricRoguelike3D/Assets/Scripts/Combat/HealthSettings.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace IsometricRoguelike.Health
+{
+    [CreateAssetMenu(fileName = "HealthSettings", menuName = "IsometricRoguelike/Combat/HealthSettings")]
+    public class HealthSettings : ScriptableObject
+    {
+        [SerializeField] private float _maxHealth;
+        [SerializeField] private float _currentHealth;
+        [SerializeField] private bool _isAlive;
+        public int MinimumHealth { get { return 0; } }
+
+        public bool IsAlive
+        {
+            get { return _isAlive; }
+            set { _isAlive = value; }
+        }
+
+        public float Health_Current
+        {
+            get { return _currentHealth; }
+            set { _currentHealth = value; }
+        }
+
+        public float Health_Max
+        {
+            get { return _maxHealth; }
+            set { _maxHealth = value; }
+        }
+
+    }
+}
